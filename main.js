007cc87b73432c64a783f63d1a2d739702db5e8f
yashbhale <yashvivekbhale@gmail.com>
2025-04-24 00:28:03 +0530

    hi

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ dotenv.config({});
 const app = express();
 const PORT = process.env.PORT|| 5001;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials:true}));
+// app.use(cors({ origin: 'http://localhost:5173', credentials:true}));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
 app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use(cookieParser());
